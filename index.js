94adedf02162c06f735e6e321b1a9ca0747303d6
David wanjohi <davidwagura83@gmail.com>
2025-03-17 18:34:08 +0300

    chore: clean code

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-function showTime()
-{
+// Function to update the clock
+function showTime() {
     var date = new Date();
     var h = date.getHours();
     var m = date.getMinutes();
     var s = date.getSeconds();
     var session = "AM";
 
-    if(h == 0){
+    if (h == 0) {
         h = 12;
     }
 
-    if(h > 12){
+    if (h > 12) {
         h = h - 12;
         session = "PM";
     }
 
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
 
     var time = h + ":" + m + ":" + s + " " + session;
-
     document.getElementById("myClockDisplay").innerText = time;
 
-    document.getElementById("myClockDisplay").textContent = time;
+    // Update the background based on the time
+    setBackground(h);
 
     setTimeout(showTime, 1000);
-
 }
 
 showTime();
